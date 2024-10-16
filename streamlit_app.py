4ba7c7c7a8c59ee384e7a48230dabe9d6341b211
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-10-16 17:41:22 +0000

    working on adding the excel file uploader

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,3 @@ uploaded_files = st.file_uploader(
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
