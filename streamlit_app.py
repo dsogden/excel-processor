44236eb0c27fceada82d8c8b8105ac051fbb16f5
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-10-16 17:52:55 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,9 @@ uploaded_files = st.file_uploader(
     "Select all files", accept_multiple_files=True
 )
 
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
+# for uploaded_file in uploaded_files:
+#     bytes_data = uploaded_file.read()
+#     st.write("filename:", uploaded_file.name)
 
 substance_list = st.file_uploader(
     "Add the substance list excel file", accept_multiple_files=False
