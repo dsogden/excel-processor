5c9bbe82ca9c768c6397ed3ff506e2927319c104
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-10-16 19:34:35 +0000

    general changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,14 +24,14 @@ if methods_list:
     # st.write('Methods loaded')
     methods = pd.read_csv(methods_list, skiprows=0)
 
-def main():
-    df_dict = {
+df_dict = {
         f.name.split('.csv')[0]: pd.read_csv(f, skiprows=1)\
             .drop(columns=['Test No', 'Status', 'Weight'])
         for f in files
     }
+
+def main():
     sorter = Sorter(df_dict)
-    st.write(df_dict)
     substance_mapper = {value[-1]: value[0] for value in substances.values}
     df = sorter.run(substance_mapper)
     columns_order = [
