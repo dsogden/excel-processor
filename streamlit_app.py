f7874b172cbb721e3a7928fd35230bada587f51c
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-10-16 19:22:48 +0000

    testing the sorter

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,13 +21,16 @@ if substance_list:
 # )
 # methods = pd.read_excel(methods_list, skiprows=0)
 if len(files) > 0:
-    df_dict = {f.name.split('.csv')[0]: pd.read_csv(f, skiprows=1) for f in files}
-    # st.write(df_dict)
-    # sorter = Sorter(df_dict)
+    df_dict = {
+        f.name.split('.csv')[0]: pd.read_csv(f, skiprows=1)\
+            .drop(columns=['Test No', 'Status', 'Weight'])
+        for f in files
+    }
+    sorter = Sorter(df_dict)
     st.write(df_dict)
-    # substance_mapper = {value[-1]: value[0] for value in substances.values}
-    # df = sorter.run(substance_mapper)
-    # st.dataframe(df.head(10))
+    substance_mapper = {value[-1]: value[0] for value in substances.values}
+    df = sorter.run(substance_mapper)
+    st.dataframe(df.head(10))
 # st.write(sorter.input_files)
 # def main():
 # csv = df.to_csv(index=False)
