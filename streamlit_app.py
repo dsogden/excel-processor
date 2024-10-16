03212669d45d0e5dd841eb3f189efc0bffcdcba3
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-10-16 18:22:29 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,12 +20,12 @@ methods_list = st.file_uploader(
 # methods = pd.read_excel(methods_list, skiprows=0)
 if len(files) > 0:
     for f in files:
-        # st.write("filename:", f.name)
+        st.write("filename:", f.name)
         df = pd.read_csv(f, skiprows=1)
         st.dataframe(df.head(10))
 
 sorter = Sorter(files)
-st.write(sorter.input_files)
+# st.write(sorter.input_files)
 # def main():
 # csv = df.to_csv(index=False)
 
