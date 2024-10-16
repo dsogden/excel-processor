cedb711dee6b990835fac64abb7b6034f73a4606
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-10-16 17:40:03 +0000

    working on adding the excel file uploader

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,16 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Medtronic Excel Processor")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-st.write('Empty')
+# st.write('Empty')
+
+uploaded_files = st.file_uploader(
+    "Choose a CSV file", accept_multiple_files=True
+)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
