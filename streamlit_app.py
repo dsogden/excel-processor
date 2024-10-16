944566e29bf1c265a137ef57a6d49a3eab359df2
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-10-16 17:47:55 +0000

    removed files and added a method to upload extra files

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+
 # from sorter import Sorter
 
 st.title("Medtronic Excel Processor")
@@ -11,3 +13,12 @@ for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
 
+substance_list = st.file_uploader(
+    "Add the substance list excel file", accept_multiple_files=False
+)
+substances = pd.read_excel(substance_list, skiprows=0)
+
+methods_list = st.file_uploader(
+    "Add the methods list excel file", accept_multiple_files=False
+)
+methods = pd.read_excel(methods_list, skiprows=0)
