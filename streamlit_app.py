00eb7b408ae47497fedc1b2e14be84f3e1840ebc
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-10-16 18:20:12 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ if len(files) > 0:
         st.dataframe(df.head(10))
 
 sorter = Sorter(files)
-st.write(sorter.df_dict)
+st.write(sorter.input_files)
 # def main():
 # csv = df.to_csv(index=False)
 
