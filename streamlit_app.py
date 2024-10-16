a21ee086ce37ef3ab73d6aadf5772d4c4426deaf
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-10-16 17:43:59 +0000

    added substance list and methods list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,13 @@
 import streamlit as st
+# from sorter import Sorter
 
 st.title("Medtronic Excel Processor")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-
-# st.write('Empty')
 
 uploaded_files = st.file_uploader(
     "Choose a CSV file", accept_multiple_files=True
 )
+
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
+
