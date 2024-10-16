4e33439fc9d02a78577fa5ea8fb00824d3853666
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-10-16 17:56:04 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-# from sorter import Sorter
+from sorter import Sorter
 
 st.title("Medtronic Excel Processor")
 
-uploaded_files = st.file_uploader(
+files = st.file_uploader(
     "Select all files", accept_multiple_files=True
 )
 
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
+for f in files:
+    st.write("filename:", f.name)
 
 substance_list = st.file_uploader(
     "Add the substance list excel file", accept_multiple_files=False
@@ -21,5 +20,8 @@ substance_list = st.file_uploader(
 methods_list = st.file_uploader(
     "Add the methods list excel file", accept_multiple_files=False
 )
-
 # methods = pd.read_excel(methods_list, skiprows=0)
+st.write(
+    files
+)
+# sorter = Sorter(files)
