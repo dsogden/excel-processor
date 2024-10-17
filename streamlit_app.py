5118839f6e0f7602d9e21ebe22bdcd3dba8544bf
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2024-10-17 12:21:55 +0000

    testing the main function for bugs

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,6 @@ substance_list = st.file_uploader(
 )
 
 if substance_list:
-    # st.write('Substances loaded')
     substances = pd.read_csv(substance_list, skiprows=0)
 
 methods_list = st.file_uploader(
@@ -17,49 +16,40 @@ methods_list = st.file_uploader(
 )
 
 if methods_list:
-    # st.write('Methods loaded')
     methods = pd.read_csv(methods_list, skiprows=0)
 
 files = st.file_uploader(
     "Select all results files (csv format)", accept_multiple_files=True
 )
 
-# df_dict = {
-#         f.name.split('.csv')[0]: pd.read_csv(f, skiprows=1)\
-#             .drop(columns=['Test No', 'Status', 'Weight'])
-#         for f in files
-#     }
-
 if len(files) > 0:
-    # test_df = pd.read_csv(files[-1], skiprows=1)
-    # st.dataframe(test_df.head(10))
     df_dict = {
         f.name.split('.csv')[0]: pd.read_csv(f, skiprows=1)\
             .drop(columns=['Test No', 'Status', 'Weight'])
         for f in files
     }
-    st.write(df_dict)
+    # st.write(df_dict)
 
 # @st.cache_data
 # def convert_df(df):
 #     return df.to_csv().encode('utf-8')
 
-# def main():
-#     sorter = Sorter(df_dict)
-#     substance_mapper = {value[-1]: value[0] for value in substances.values}
-#     df = sorter.run(substance_mapper)
-#     columns_order = [
-#         'Material(s)', 'Substance', 'CAS No.', 'Method', 'Result (mg/kg)'
-#     ]
-#     return df.merge(
-#         methods, how='left', left_on='Test', right_on='Method Name'
-#     ).merge(substances, how='left', on='Substance')[columns_order]
+def main():
+    sorter = Sorter(df_dict)
+    substance_mapper = {value[-1]: value[0] for value in substances.values}
+    df = sorter.run(substance_mapper)
+    columns_order = [
+        'Material(s)', 'Substance', 'CAS No.', 'Method', 'Result (mg/kg)'
+    ]
+    return df.merge(
+        methods, how='left', left_on='Test', right_on='Method Name'
+    ).merge(substances, how='left', on='Substance')[columns_order]
 
-# if __name__ == '__main__':
-#     if len(files) > 0:
-#         result = main()
-#         st.write('Example of the first 5 lines of file')
-#         st.dataframe(result.head(5))
+if __name__ == '__main__':
+    if len(files) > 0:
+        result = main()
+        st.write('Example of the first 5 lines of file')
+        st.dataframe(result.head(5))
 
 #         # Create a download button
 #         csv = convert_df(result)
